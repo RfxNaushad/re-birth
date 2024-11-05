3c1a7da4412b3e6913039ff89a9505017b895800
RfxNaushad <web.naushad@gmail.com>
2024-11-05 11:48:49 +0600

    Feat: card stack

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import BookACall from "./components/BookACall";
 import ExpertiseSection from "./components/ExpertiseSection";
 import FeaturedProject from "./components/FeaturedProject";
@@ -9,21 +9,20 @@ import MetricsSection from "./components/MetricsSection";
 import ProjectsList from "./components/ProjectList";
 // import TestimonialSlider from "./components/TestimonialSlider";
 import TextSection from "./components/TextSection";
-import gsap from "gsap";
-// import LenisWrapper from "./components/LenisWraper";
+
 
 
 function App() {
-    useEffect(() => {
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: '.main',
-                start: 'top -30%',
-                end: 'bottom 30%',
-                scrub: true,
-            }
-        }).fromTo('.main', { y: '0%' }, { y: '-15%', })
-    }, [])
+    // useEffect(() => {
+    //     gsap.timeline({
+    //         scrollTrigger: {
+    //             trigger: '.main',
+    //             start: 'top -30%',
+    //             end: 'bottom 30%',
+    //             scrub: true,
+    //         }
+    //     }).fromTo('.main', { y: '0%' }, { y: '-15%', })
+    // }, [])
     return (
         <div>
             <HeroSection />
